f000f12bd97c37634410bdc240faad77a2193228
Amanda Chen <achen01@stuy.edu>
2019-10-24 13:18:11 -0400

    moved database funtions into another file

--- app.py
+++ app.py
@@ -8,19 +8,10 @@
 from flask import Flask, render_template, request, redirect, url_for,session, flash, get_flashed_messages
 import os
 import sqlite3
+import databasing
 
 #the sqlite part aka db_builder.py
-data="data.db"
-db=sqlite3.connect(data)
-c=db.cursor()
-command="CREATE TABLE if not EXISTS Story_List(ID INTEGER, Title TEXT, Story TEXT)"
-c.execute(command)
-command="CREATE TABLE if not EXISTS Edits(ID INTEGER,Edit TEXT,Timestamp TIMESTAMP, Username TEXT)"
-c.execute(command)
-command="CREATE TABLE if not EXISTS Accounts(Username TEXT,Password TEXT)"
-c.execute(command)
-db.commit()
-db.close()
+databasing.buildDB()
 
 #the flask part
 app = Flask(__name__)
@@ -32,30 +23,18 @@ def signuppage():
     return render_template('join.html'); # the method to add account
 @app.route("/join",methods=['POST'])
 def create():
-    data="data.db"
-    db=sqlite3.connect(data)
-    c=db.cursor()
 
     username=request.form["new_user"]
     print(username)
     password=request.form["new_password"]
 
-    command = "SELECT count(*) FROM Accounts WHERE username=\"{}\";"
-    countWithUser = c.execute( command.format(username) )
-    data = c.fetchone()[0] # i dont get this line? gonna try and figure it out later
-    print(data)
+    data = databasing.verifyUser(username)
     if(data == 0):
-        command="INSERT INTO Accounts VALUES(\"{}\",\"{}\")"
-        c.execute(command.format(username,password))
-
-        db.commit()
-        db.close()
+        databasing.addUser(username,password)
         flash('You have successfully created your account, and logged in!')
         session['username']=username
         return redirect(url_for("mystories"))
     else:
-        db.commit()
-        db.close()
         flash('Username is taken. Please try again with another username.')
         return redirect(url_for("root"))
     return render_template('homepage.html')
@@ -81,22 +60,14 @@ def authenticate():
     username = request.form['username']
     password = request.form['password']
 
-    db = sqlite3.connect('data.db')
-    c = db.cursor()
-
-    command = 'select count(*) from Accounts where username="{}" and password="{}"'
-    c.execute(command.format(username,password))
-    result = c.fetchone()[0]
+    result = databasing.verifyUser(username)
     print('huh?')
+
     if(result == 1): # username and password valid
         session['username'] = username
-        db.commit()
-        db.close()
         flash('You have successfully logged in!')
         return redirect(url_for('mystories'))
     else:
-        db.commit()
-        db.close()
         flash('Incorrect username or password.')
         return redirect(url_for('root'))
     return render_template('out.html',
@@ -108,8 +79,10 @@ def authenticate():
 @app.route("/logout")
 def logout():
     app.secret_key = os.urandom(32)
-    session.pop('user') #removes session info
-    session.pop('pass')
+    #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    #print(session)
+    #print(request.form)
+    session.pop('username') #removes session info
     return redirect(url_for('root'))
 
 
@@ -143,35 +116,16 @@ def modifypage():
     print(request.method)
     storyID = request.args['story_id']
 
-    db = sqlite3.connect('data.db')
-    c = db.cursor()
+    result = databasing.getStory(storyID)
 
-    command = '''
-    select
-        Story_List.Title,
-        Edits.Edit,
-        Edits.Timestamp,
-        Edits.Username
-    from
-        Story_List
-    left join
-        Edits using (ID)
-    where Story_List.ID={}
-    order by Edits.Timestamp desc;
-    '''
-    c.execute(command.format(storyID,storyID))
-    result = c.fetchone()
     print(result)
+
     if(result is None):
         # case: invalid id, no story with such id
-        db.commit()
-        db.close()
 
         flash('Error occured: no such story with given id')
         return redirect(url_for('mystories'))
 
-    db.commit()
-    db.close()
 
     print(result[0])
     return render_template(
