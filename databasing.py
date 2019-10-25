490bc14987cf116642c754ce7aa6323d6c98ea08
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-10-24 23:02:39 -0400

    restored methods that for some reason disappeared from merge (AMANDAS CODE)

--- databasing.py
+++ databasing.py
@@ -136,6 +136,79 @@ def getStory(storyID):
 
     return result
 
+def userStories(user):
+    data="data.db"
+    db=sqlite3.connect(data)
+    c=db.cursor()
+
+    command='''
+        SELECT
+            ID,
+            Title,
+            Story
+        FROM
+            Story_List
+        INNER JOIN
+            Edits using (ID)
+        WHERE
+            Username=\"{}\";
+        '''
+    c.execute( command.format(user) )
+    results = c.fetchall()
+
+    db.commit()
+    db.close()
+
+    return results
+
+def otherStories(user):
+    data="data.db"
+    db=sqlite3.connect(data)
+    c=db.cursor()
+
+    # command='''
+    #     SELECT
+    #         ID,
+    #         Title,
+    #         Story
+    #     FROM
+    #         Story_List
+    #     INNER JOIN
+    #         Edits using (ID)
+    #     WHERE
+    #         Username!=\"{}\";
+    #     '''
+        #***********THIS DOES NOT WORK!!***********
+    c.execute( command.format(user) )
+    results = c.fetchall()
+
+    db.commit()
+    db.close()
+
+    return results
+
+def addEdit(storyID,edit,time,user):
+    data="data.db"
+    db=sqlite3.connect(data)
+    c=db.cursor()
+
+    command="INSERT INTO Accounts VALUES(\"{}\",\"{}\",\"{}\",\"{}\")"
+    c.execute( command.format(storyID,edit,time,user) )
+
+    db.commit()
+    db.close()
+
+def addStory(storyID,title,story):
+    data="data.db"
+    db=sqlite3.connect(data)
+    c=db.cursor()
+
+    command="INSERT INTO Accounts VALUES(\"{}\",\"{}\",\"{}\")"
+    c.execute( command.format(storyID,title,story) )
+
+    db.commit()
+    db.close()
+
 
 def addedittodatabase(username,id,editText):
     db = sqlite3.connect('data.db')
